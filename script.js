087616b0f9b2f18a00c2e5780f3ad563a9de2335
Bertalia <144839839+OtavioBertalia@users.noreply.github.com>
2025-09-01 10:56:18 -0300

    alteração diminiuir fonte

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded',function(){const aumentaFonteBotao=
     aumentaFonteBotao.addEventListener('click',function(){
     tamanhoAtualFonte +=0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
+    )};
     diminuiFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
