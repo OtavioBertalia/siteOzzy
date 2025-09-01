39205e3b1c07c8cddc08d3fe1619858c638c91b8
Bertalia <144839839+OtavioBertalia@users.noreply.github.com>
2025-09-01 10:58:46 -0300

    alteracao

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ document.addEventListener('DOMContentLoaded',function(){const aumentaFonteBotao=
     tamanhoAtualFonte +=0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     )};
-    diminuiFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
-    });
+   let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click',function(){
+    tamanhoAtualFonte -=0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    )}; 
 });
