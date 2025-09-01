a6ea328668557e3f682a5d8594e2e8804b323371
Bertalia <144839839+OtavioBertalia@users.noreply.github.com>
2025-09-01 10:53:24 -0300

    alteraçao diminuicao da fonte

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded',function(){const aumentaFonteBotao=
     tamanhoAtualFonte +=0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
+    diminuiFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 });
